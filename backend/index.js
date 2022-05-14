0e5847b9a7eee92a2efd74b26b9508560ac7ef0a
navalinovian <viansatria57@gmail.com>
2022-05-14 19:57:28 +0700

    category endpoint

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 const db = require('./queries.js')
-  
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -12,3 +15,6 @@ app.listen(port, () => {
 })
 
 app.get('/category', db.getCategory)
+app.post('/category', db.createCategory)
+app.put('/category/:id', db.updateCategory)
+app.delete('/category/:id', db.deleteCategory)
--- backend/queries.js
+++ backend/queries.js
@@ -6,7 +6,8 @@ const pool = new Pool({
   password: '1q2w3e4r',
   port: 5432,
 })
-
+/* PRODUCTION SCHEMA */
+    /* CATEGORY */
 const getCategory = (request, response) =>{
     pool.query('SELECT * FROM production.category ORDER by id ASC', (error, result)=>{
         if (error) {
@@ -15,7 +16,58 @@ const getCategory = (request, response) =>{
         response.status(200).json(result.rows);
     })
 }
+const getCategoryById = (request, response) =>{
+    const id = parseInt(request.params.id)
+    pool.query('SELECT * FROM production.category WHERE id=$1',[id], (error, result)=>{
+        if (error) {
+            throw error
+        }
+        response.status(200).json(result.rows);
+    })
+}
+
+const createCategory = (request, response) => {
+    const { name } = request.body
+    const now = new Date()
+    pool.query('INSERT INTO production.category (name, created_at, updated_at, deleted_at) VALUES ($1, $2, $3, $4) RETURNING id', [name, now, now, null], (error, result) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).send(`Category added with ID: ${result.rows[0].id}`)
+    })
+  }
+
+const updateCategory = (request, response) => {
+    const id = parseInt(request.params.id)
+    const { name } = request.body
+    const now = new Date()
+    pool.query(
+      'UPDATE production.category SET name = $1, updated_at = $2 WHERE id = $3',
+      [name, now, id],
+      (error, result) => {
+        if (error) {
+          throw error
+        }
+        response.status(200).send(`Category with ID: ${id} has been modified`)
+      }
+    )
+  }
+
+const deleteCategory = (request, response) => {
+    const id = parseInt(request.params.id)
+    const now = new Date()
+    pool.query('UPDATE  production.category SET deleted_at = $1 WHERE id = $2', [now,id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`Category with ID: ${id} has been deleted`)
+    })
+  }
 
 module.exports = {
     getCategory,
+    getCategoryById,
+    createCategory,
+    updateCategory,
+    deleteCategory
   }
