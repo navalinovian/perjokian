9719d2622ae4c64de35ae6ed39cf3d53cccf1370
navalinovian <viansatria57@gmail.com>
2022-05-31 08:33:07 +0700

    invoice attributes

--- backend/controller/InvoiceController.js
+++ backend/controller/InvoiceController.js
@@ -3,8 +3,22 @@ const { Invoice, InvoiceItem, User, Product, Category } = require("../models");
 
 exports.invoiceIndex = async (req, res) => {
     try {
-        const invoices = await Invoice.findAll({
-            include: [User, Product]
+        const invoice = await Invoice.findOne({
+            where: {
+                id: uuid
+            },
+            include: {
+                model: Product,
+                attributes: ['id','name', 'price'],
+                include:{
+                    model:Category,
+                    attributes: ['name'],
+                },
+                through:{
+                    attributes:['quantity']
+                    
+                }   
+            }
         })
         return res.json(invoices)
     } catch (error) {
@@ -74,60 +88,3 @@ exports.invoiceCreate = async (request, res) => {
     }
 }
 
-exports.productUpdate = async (req, res) => {
-    const id = req.params.uuid
-    const { name, price, stock, category_id } = req.body
-    try {
-        const category = await Category.findOne({
-            where: {
-                id: category_id
-            }
-        })
-
-        const product = await Product.update({
-            name: name,
-            price: price,
-            stock: stock,
-            categoryId: category_id
-        }, {
-            where: {
-                id: id
-            }
-        })
-        return res.json(product)
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ error: 'Something went wrong' })
-    }
-}
-
-exports.productSoftDelete = async (req, res) => {
-    const id = req.params.uuid
-    try {
-        const product = await Product.destroy({
-            where: {
-                id: id
-            }
-        })
-        return res.json(product)
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ error: 'Something went wrong' })
-    }
-}
-
-exports.productHardDelete = async (req, res) => {
-    const id = req.params.uuid
-    try {
-        const product = await Product.destroy({
-            where: {
-                id: id
-            },
-            force: true
-        })
-        return res.json(product)
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ error: 'Something went wrong' })
-    }
-}
